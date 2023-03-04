99df30747106c31a6e89daa226bf0d0c832c0247
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-03-04 09:13:55 -0500

    n

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
 import fs from "fs";
-import cripto from "crypto";
+import crypto from "crypto";
 import * as dotenv from "dotenv";
 
 
@@ -11,13 +11,13 @@ dotenv.config();
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 8000;
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = "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";
 
 const URL = 'https://api.openai.com/v1/images/generations'
 
-//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
+
 
 //middlewares
 app.use(cors());
@@ -34,19 +34,41 @@ app.get("/test", (req, res) => {
 
 app.post("/generate-image", async (req, res) => {
     const prompt = req.body.prompt;
-
-    Headers = {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${API_KEY}`  
+    console.log(prompt);
+    const headers = {
+        "Content-Type" : "application/json",
+        Authorization : `Bearer ${API_KEY}`  
     };
     try {
         if (!!!prompt){
             throw Error('no prompt found')
         }
-        const response = await fetch()
+        const response = await fetch(URL, {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify({prompt, size: "256x256"})
+        })
+        const data = await response.json()
+        console.log(data);
+        const imageURL = data.data[0].url;
+
+        const imageResponse = await fetch(imageURL);
+        const imageBuffer = await imageResponse.arrayBuffer();
+        const buffer = Buffer.from(imageBuffer);
+
+        const id = crypto.randomBytes(20).toString('hex');
+        const filename = `${id}.jpg`;
+        const pathname = "public/images/";
+
+        fs.writeFileSync(`${pathname}${filename}`, buffer);
+        console.log("Image save locally");
+
+        res.setHeader("Content-Type", "application/json");
+        res.json({ filename })
+
 
     } catch (error) {
-        console.log(`error generate image ${error.message}`);
+        console.log(`error generating image ${error.message}`);
         res.sendStatus(500)
     }
 })
---
+++ public/app.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("form")
+const input = document.querySelector("#prompt")
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const data = { prompt: input.value }
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    };
+
+    fetch("/generate-image", options)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if (data?.filename) {
+                const image = document.createElement("img")
+                image.src = "./images/" + data?.filename;
+                document.querySelector("#results").appendChild(image)
+            }
+        })
+})
